09904dfde790c37ed0049ad66657a36a71c55d7f
Hasensab <r161135@rguktrkv.ac.in>
2023-12-15 18:53:03 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from app.models import *
 
 # Create your views here.
+def display_dept(request):
+    DTO=Dept.objects.all()
+    d={'dept':DTO}
+    return render(request,'dept.html',d)
+def display_emp(request):
+    ETO=Emp.objects.all()
+    d={'emp':ETO}
+    return render(request,'emp.html',context=d)
+
+def display_author(request):
+    ATO=Author.objects.all()
+    d={'author':ATO}
+    return render(request,'author.html',d)
+
