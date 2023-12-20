f8e55cae879fd1c4295dd63eaf71b8b33524e1c8
Hasensab <r161135@rguktrkv.ac.in>
2023-12-20 18:41:30 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Dept(models.Model):
     DEPTNO=models.IntegerField(primary_key=True)
-    DNAME=models.CharField(max_length=100,)
+    DNAME=models.CharField(max_length=100)
     LOC=models.CharField(max_length=100,unique=True)
     def __str__(self):
         return self.DNAME
--- app/views.py
+++ app/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from app.models import *
-from django.http import HttpResponse
+from django.db.models import Q
+
 # Create your views here.
 def display_dept(request):
     DTO=Dept.objects.all()
+    DTO=Dept.objects.filter(DEPTNO=20)
     d={'dept':DTO}
     return render(request,'dept.html',d)
 def display_emp(request):
     ETO=Emp.objects.all()
+    ETO=Emp.objects.filter(DEPTNO=10,JOB='cricket')
+    ETO=Emp.objects.filter(Q(DEPTNO=10) & Q(JOB='cricket'))
+    ETO=Emp.objects.filter(Q(DEPTNO=10) | Q(JOB='cricket'))
+    ETO=Emp.objects.filter(JOB='kabaddi',SAL__gte='50000')
+    ETO=Emp.objects.filter(Q(JOB='kabaddi') & Q(SAL__gte='50000'))
+    ETO=Emp.objects.filter(Q(JOB='kabaddi') | Q(SAL__gte='50000'))
+    
     d={'emp':ETO}
     return render(request,'emp.html',context=d)
 
@@ -16,8 +25,5 @@ def display_author(request):
     d={'author':ATO}
     return render(request,'author.html',d)
 
-def insert_data(request):
-    a = int(input('Enter data: '))
-    d = {'a':a}
-    return HttpResponse('<h1>{{a}}</h1>')
+
 
